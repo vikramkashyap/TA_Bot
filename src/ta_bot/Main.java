48004157077d02fd38d860e1b0df16e9f28c3095
U-BSD\301968 <301968@SHS-10DCE0F51.beaverton.k12.or.us>
2018-11-16 14:02:49 -0800

    trying to convert to eclipse project

---
+++ src/ta_bot/Main.java
@@ -0,0 +1,55 @@
+package ta_bot;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	int iter = 0;
+	int trainingIndex = 0;
+	Scanner scan = new Scanner(System.in);
+	String input = "placeholder";
+	NeuralNetwork net;
+	
+	public Main() {
+		net = new NeuralNetwork();
+		double[] data = new double[net.getNumInputs()];
+		
+		while(iter <= 8000000){
+			net.train(trainingIndex);
+			if(iter % 100000 == 0){
+				System.out.println("ITERATION: " + iter);
+				net.displayWeights();
+				net.displayResult(false);
+			}
+			iter++;
+			trainingIndex++;
+			if(trainingIndex == net.trainingData.length){
+				trainingIndex = 0;
+			}
+		}
+		
+		while(true){
+			System.out.println();
+			for(int i = 0; i < data.length; i++){
+				System.out.print("Enter data point " + (i+1) + ": ");
+				input = scan.nextLine();
+				try{
+					data[i] = Double.parseDouble(input);
+					System.out.println(data[i]);
+				}
+				catch (Exception e){
+					System.out.println("Enter a double!");
+					i--;
+				}
+			}
+			net.run(data);
+		}	
+	}
+
+	public static void main(String[] args) {
+		new Main();
+
+	}
+
+}
+
---
+++ src/ta_bot/Neuron.java
@@ -0,0 +1,114 @@
+package ta_bot;
+
+//Nathan Purwosumarto - August 2017
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Neuron {
+	
+	static Random random = new Random();
+	
+	int numInputs;
+	final double rate = 0.001;
+	
+	double[] inputs;
+	double[] weights;
+	double lastOutput;
+	double[] previousWeightDeltas;
+	double biasWeight = -1;
+	double momentumWeight = 0;
+	
+	public void mutate(){
+		for (int i =0; i<weights.length; i++){
+			if(Math.random()<.04)inputs[i]=inputs[i]+10*(Math.random()-.5);
+		}
+		if(Math.random()<.4)biasWeight+=10*(Math.random()-.5);
+	}
+	public Neuron(int newInputs){
+		numInputs = newInputs;
+		inputs = new double[numInputs];
+		weights = new double[numInputs];
+		previousWeightDeltas = new double[numInputs];
+		//initializes random weights between -2.0 to 2.0
+		for(int i = 0; i < numInputs; i++){
+			weights[i] = 2 * random.nextDouble() - 1;
+		}	
+	}
+	
+	public void setInputs(double[] data){
+		for (int i = 0; i < inputs.length; i++) {
+			inputs[i] = data[i];
+			
+		}
+	}
+	
+	public double calculateOutput(){
+		double total = 0;
+		for(int i = 0; i < numInputs; i++){
+			total += inputs[i] * weights[i];
+		}
+		total += 1 * biasWeight;
+		total = sigmoidFunction(total);
+		lastOutput = total;
+		
+		return total;
+	}
+	
+	public double updateErrorLastLayer(double trueValue, double error){
+		double residual = trueValue - lastOutput;
+		error += residual * (lastOutput * (1 - lastOutput));
+		return error;
+		
+		
+	}
+	
+	
+	public double calculateResidual(double truevalue){
+		return truevalue - lastOutput;
+	}
+	
+	public double[] updateWeights(double contribution){
+		double error = contribution * (lastOutput * (1 - lastOutput));
+		
+		//calculate previous layer contribution
+		double[] previousLayerContribution = new double[weights.length];
+		for (int i = 0; i < weights.length; i++) {
+			previousLayerContribution[i] = weights[i];
+		}
+		for (int i = 0; i < previousLayerContribution.length; i++) {
+			previousLayerContribution[i] *= error;
+		}
+		
+		//update actual weights
+		for(int i = 0; i < weights.length; i++){
+			weights[i] += (rate * error * inputs[i]) + (previousWeightDeltas[i] * momentumWeight);
+			previousWeightDeltas[i] = rate * error * inputs[i];
+		}
+		biasWeight += rate * error;
+		
+		return previousLayerContribution;
+		
+	}
+	public Neuron clone(){
+		Neuron n = new Neuron(numInputs);
+		n.weights = weights;
+		n.biasWeight=biasWeight;
+		n.momentumWeight=momentumWeight;
+		n.inputs = inputs;
+		n.lastOutput=lastOutput;
+		n.previousWeightDeltas=previousWeightDeltas;
+		return n;
+	}
+	public double sigmoidFunction(double x){
+		return (1/ (1 + Math.exp(-x)));
+	}
+	
+	public void printWeights(){
+		for (int i = 0; i < weights.length; i++) {
+			System.out.println(weights[i]);
+		}
+	}
+		
+
+}
